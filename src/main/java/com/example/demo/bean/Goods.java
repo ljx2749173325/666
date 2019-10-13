fe86cc9c717ab4172f25b591eb0812c876226732
ljx <2749173325@qq.com>
2019-10-13 20:36:06 +0800

    123

---
+++ src/main/java/com/example/demo/bean/Goods.java
@@ -0,0 +1,60 @@
+package com.example.demo.bean;
+
+public class Goods {
+
+	private int gid;
+	private String gname;
+	private double price;
+	// 百分比
+	private double percent;
+
+	public Goods(int gid, String gname, double price, double percent) {
+		super();
+		this.gid = gid;
+		this.gname = gname;
+		this.price = price;
+		this.percent = percent;
+	}
+
+	public Goods() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public int getGid() {
+		return gid;
+	}
+
+	public void setGid(int gid) {
+		this.gid = gid;
+	}
+
+	public String getGname() {
+		return gname;
+	}
+
+	public void setGname(String gname) {
+		this.gname = gname;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public double getPercent() {
+		return percent;
+	}
+
+	public void setPercent(double percent) {
+		this.percent = percent;
+	}
+
+	@Override
+	public String toString() {
+		return "Goods [gid=" + gid + ", gname=" + gname + ", price=" + price + ", percent=" + percent + "]";
+	}
+}
